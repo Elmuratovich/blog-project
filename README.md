e88ec2bab5cc631ea05b5436f47a0ec819f26983
Elmuratovich <makhmudkuzikaev@gmail.com>
2022-05-06 17:28:44 +0600

    rm database_url from git

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blog-project
+
+Add your own Database String in the .env file. You can use the free database from www.elephantsql.com
--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: [],
   root: true,
   env: {
     node: true,
